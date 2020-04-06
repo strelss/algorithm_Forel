8846da540d1559650f5c84cf1c3c745fcf5dfe35
Maxim <pravohranitel@yandex.ru>
2020-04-06 11:22:46 +0300

    add module of vizualization

--- alg_forel/show_picture.py
+++ alg_forel/show_picture.py
@@ -1,17 +1,29 @@
 import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
+
+class Picture:
+    def __init__(self, centroids, points, radius):
+        self.centroids = centroids
+        self.data = points
+        self.radius = radius
 
 
-
-def display(data, centroids):
-    data_x = []
-    data_y = []
-    centroids_x = []
-    centroids_y = []
-    for point in data:
-        data_x.append(point[0])
-        data_y.append(point[1])
-    for point in centroids:
-        centroids_x.append(point[0])
-        centroids_y.append(point[1])
-    plt.plot(data_x, data_y)
-    plt.show()
+    def display(self):
+        self.data_x = []
+        self.data_y = []
+        self.centroids_x = []
+        self.centroids_y = []
+        fig = plt.figure()
+        pic = fig.add_subplot(111)
+        for point in self.data:
+            for item in point:
+                self.data_x.append(item[0])
+                self.data_y.append(item[1])
+        pic.scatter(self.data_x, self.data_y)
+        for point in self.centroids:
+            self.centroids_x.append(point[0])
+            self.centroids_y.append(point[1])
+            self.circle = Circle((point[0], point[1]), self.radius, fill=False,  edgecolor='b')
+            pic.add_artist(self.circle)
+        pic.scatter(self.centroids_x, self.centroids_y, marker='x')
+        plt.show()
--- main.py
+++ main.py
@@ -1,46 +1,6 @@
 from alg_forel import forel
+from alg_forel import show_picture as s_pic
 import numpy as np
-
-
-import matplotlib.pyplot as plt
-import pylab
-from matplotlib.patches import Circle
-
-class Picture:
-    def __init__(self, centroids, points, radius):
-        self.centroids = centroids
-        self.data = points
-        self.radius = radius
-
-
-    def display(self):
-        self.data_x = []
-        self.data_y = []
-        self.centroids_x = []
-        self.centroids_y = []
-        fig = plt.figure()
-        pic = fig.add_subplot(111)
-        for point in self.data:
-            for item in point:
-                self.data_x.append(item[0])
-                self.data_y.append(item[1])
-        pic.scatter(self.data_x, self.data_y)
-        for point in self.centroids:
-            self.centroids_x.append(point[0])
-            self.centroids_y.append(point[1])
-            self.circle = Circle((point[0], point[1]), self.radius, fill=False)
-            pic.add_artist(self.circle)
-        pic.scatter(self.centroids_x, self.centroids_y, marker='x')
-
-        # circle = Circle((5, 5), self.radius, fill=False)
-
-        plt.show()
-
-
-
-        print('---------')
-        print(self.data_x, self.data_y)
-
 
 
 
@@ -57,7 +17,7 @@ class Picture:
 
 points = np.array([(7, 9), (6, 9), (6, 8), (2, 3), (1, 3), (1, 2)])
 
-radius = 2
+radius = 1.5
 
 a = forel.Forel(points, radius)
 
@@ -69,5 +29,5 @@ print(a[1])
 
 size = [50, 100]
 
-b = Picture(a[0], a[1], radius)
+b = s_pic.Picture(a[0], a[1], radius)
 b = b.display()
